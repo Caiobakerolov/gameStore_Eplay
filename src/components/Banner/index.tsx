ed7d9745d36161b1a9cdaf81cc3f1ba373d4df8f
Caio Bakerolov <caiobakerolov@icloud.com>
2024-04-24 14:00:36 +1000

    config button, Tag and adjust styles banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,15 +1,27 @@
 import { Image, Title, Price } from './styles'
 import bannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => (
   <>
     <Image style={{ backgroundImage: `url(${bannerImg})` }}>
       <div className="container">
-        <Title>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Title>
-        <Price>
-          From <span>U$$99.00</span> <br />
-          to only U$$49.99
-        </Price>
+        <Tag size="big">Highlight of the Day</Tag>
+        <div>
+          <Title>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Title>
+          <Price>
+            From <span>U$$99.00</span> <br />
+            to only U$$49.99
+          </Price>
+        </div>
+        <Button
+          type="link"
+          to="/product"
+          title="click here to take advantage of this offer"
+        >
+          Enjoy
+        </Button>
       </div>
     </Image>
   </>
--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { TagContainer } from '../Tag/styles'
 
 export const Image = styled.div`
   width: 100%;
@@ -7,13 +8,26 @@ export const Image = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   font-weight: bold;
-  padding-top: 340px;
+
+  .container {
+    position: relative;
+    padding-top: 340px;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+
+  ${TagContainer} {
+    position: absolute;
+    top: 32px;
+  }
 `
 
 export const Title = styled.h2`
   font-size: 36px;
   max-width: 450px;
 `
+
 export const Price = styled.p`
   font-size: 24px;
   margin-top: 24px;
