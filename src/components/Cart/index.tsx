edb35cf412c3745b453dde88f56e959f8418530b
Caio Bakerolov <caiobakerolov@icloud.com>
2024-08-03 11:12:28 +1000

    add CART

---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,16 @@
+const Cart = () => (
+  <div>
+    <div></div>
+    <aside>
+      <ul>
+        <li>
+          <h3>Name game</h3>
+        </li>
+      </ul>
+      <p>2 game(s) in cart</p>
+      <p></p>
+    </aside>
+  </div>
+)
+
+export default Cart
