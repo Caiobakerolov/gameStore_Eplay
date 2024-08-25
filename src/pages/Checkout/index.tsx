d88ba045259515786d5da30d833f1c372b415419
Caio Bakerolov <caiobakerolov@icloud.com>
2024-08-25 12:00:07 +1000

    initiation of the application of formik and yup

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -7,9 +7,30 @@ import { InputGroup, Row, TabButton } from './styles'
 
 import creditCard from '../../assets/images/credit-card.png'
 import bankSlip from '../../assets/images/barcode.png'
+import { useFormik } from 'formik'
 
 const Checkout = () => {
   const [payWithCard, setPayWithCard] = useState(false)
+
+  const form = useFormik({
+    initialValues: {
+      fullName: '',
+      cpf: '',
+      deliveryEmail: '',
+      confirmDeliveryEmail: '',
+      cardOwner: '',
+      cpfOwner: '',
+      cardDisplayName: '',
+      cardNumber: '',
+      expiresMonth: '',
+      expiresYear: '',
+      cardCode: '',
+      installments: 1
+    },
+    onSubmit: (value) => {
+      console.log(value)
+    }
+  })
 
   return (
     <>
@@ -19,26 +40,42 @@ const Checkout = () => {
             <Row>
               <InputGroup>
                 <label htmlFor="fullName">Full name</label>
-                <input id="fullName" type="text" />
-              </InputGroup>
-              <InputGroup>
-                <label htmlFor="email">Email</label>
-                <input id="email" type="email" />
+                <input
+                  id="fullName"
+                  type="text"
+                  name="fullName"
+                  value={form.values.fullName}
+                />
               </InputGroup>
               <InputGroup>
                 <label htmlFor="cpf">Cpf</label>
-                <input id="cpf" type="text" />
+                <input
+                  id="cpf"
+                  type="text"
+                  name="cpf"
+                  value={form.values.cpf}
+                />
               </InputGroup>
             </Row>
             <h3 className="margin-top">Delivery date - digital content</h3>
             <Row>
               <InputGroup>
                 <label htmlFor="deliveryEmail">E-mail</label>
-                <input type="email" id="deliveryEmail" />
+                <input
+                  type="email"
+                  id="deliveryEmail"
+                  name="deliveryEmail"
+                  value={form.values.deliveryEmail}
+                />
               </InputGroup>
               <InputGroup>
                 <label htmlFor="confirmDeliveryEmail">Confirm Email</label>
-                <input type="email" id="confirmDeliveryEmail" />
+                <input
+                  type="email"
+                  id="confirmDeliveryEmail"
+                  name="confirmDeliveryEmail"
+                  value={form.values.confirmDeliveryEmail}
+                />
               </InputGroup>
             </Row>
           </>
@@ -65,42 +102,81 @@ const Checkout = () => {
                   <Row marginTop="24px">
                     <InputGroup>
                       <label htmlFor="cardOwner">Cardholder name</label>
-                      <input type="text" id="cardOwner" />
+                      <input
+                        type="text"
+                        id="cardOwner"
+                        name="cardOwner"
+                        value={form.values.cardOwner}
+                      />
                     </InputGroup>
                     <InputGroup>
                       <label htmlFor="cpfOwner">Cardholder CPF</label>
-                      <input type="text" id="cpfOwner" />
+                      <input
+                        type="text"
+                        id="cpfOwner"
+                        name="cpfOwner"
+                        value={form.values.cpfOwner}
+                      />
                     </InputGroup>
                   </Row>
                   <Row marginTop="24px">
                     <InputGroup>
                       <label htmlFor="cardDisplayName">Card display name</label>
-                      <input type="text" id="cardDisplayName" />
+                      <input
+                        type="text"
+                        id="cardDisplayName"
+                        name="cardDisplayName"
+                        value={form.values.cardDisplayName}
+                      />
                     </InputGroup>
                     <InputGroup>
                       <label htmlFor="cardNumber">Card number</label>
-                      <input type="text" id="cardNumber" />
+                      <input
+                        type="text"
+                        id="cardNumber"
+                        name="cardNumber"
+                        value={form.values.cardNumber}
+                      />
                     </InputGroup>
                     <InputGroup maxWidth="123px">
                       <label htmlFor="expiresMonth">Due month</label>
-                      <input type="text" id="expiresMonth " />
+                      <input
+                        type="text"
+                        id="expiresMonth "
+                        name="expiresMonth"
+                        value={form.values.expiresMonth}
+                      />
                     </InputGroup>
                     <InputGroup maxWidth="123px">
                       <label htmlFor="expiresYear">Due year</label>
-                      <input type="text" id="expiresYear" />
+                      <input
+                        type="text"
+                        id="expiresYear"
+                        name="expiresYear"
+                        value={form.values.expiresYear}
+                      />
                     </InputGroup>
                     <InputGroup maxWidth="48px">
-                      <label htmlFor="cvv">CVV</label>
-                      <input type="text" id="cvv" />
+                      <label htmlFor="cardCode">CVV</label>
+                      <input
+                        type="text"
+                        id="cardCode"
+                        name="cardCode"
+                        value={form.values.cardCode}
+                      />
                     </InputGroup>
                   </Row>
                   <Row marginTop="24px">
                     <InputGroup maxWidth="140px">
                       <label htmlFor="installments">installment</label>
-                      <select name="" id="">
-                        <option value="">1x of R$200,00</option>
-                        <option value="">2x of R$200,00</option>
-                        <option value="">3x of R$200,00</option>
+                      <select
+                        id="installments"
+                        name="installments"
+                        value={form.values.installments}
+                      >
+                        <option>1x of R$200,00</option>
+                        <option>2x of R$200,00</option>
+                        <option>3x of R$200,00</option>
                       </select>
                     </InputGroup>
                   </Row>
